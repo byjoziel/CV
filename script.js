06cd1702b3d2708db8dd70384c57c9f787b4c8b1
joziel <candav09@gmail.com>
2022-06-16 21:20:32 -0600

    actualizacion

--- script.js
+++ script.js
@@ -23,4 +23,36 @@ function descarga(){
 var clickbtn = document.getElementById("btn");
 clickbtn.onclick = descarga;
 
-var doc = new jsPDF()
+var elementHTML = document.getElementById.getElementById("elementH");
+var doc = new jsPDF("P" + "PT" + "LETTER");
+var margin = 10;
+var scale = (doc.internel.pageSize.width - margin * 2) / 
+document.container.scrollWidth;
+doc.html(document.container, {
+    x: margin,
+    y:margin,
+    html2canvas: {
+        scale: scale, 
+    }, 
+    callback: function(doc){
+        doc.output("dataurlnewwindow", {
+            filename: "sample-document.pdf"
+        });
+    }
+}) 
+var elementHTML = $ ("container").html();
+var epecialElementHanlers ={
+    "#elementH" : function (element, renderer) {
+        return true;
+    }
+}
+doc.fromHTML (elementHTML, 15, 15, {
+    "width":170, "elementHandlers": especialElementHandlers
+});
+
+doc.addPage();
+doc.text(20, 20, 'CurriculumCv');
+doc.setFontSize(18);
+doc.save ("sample-document.pdf");
+
+
